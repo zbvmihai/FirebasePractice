a24579aa898efdb2580506e0e5cf86b88ee013ce
Zabi <zbvmihai@gmail.com>
2023-03-22 17:22:00 +0000

    Added functionality for add user

--- app/src/main/java/com/zabava/firebase/MainActivity.kt
+++ app/src/main/java/com/zabava/firebase/MainActivity.kt
@@ -3,9 +3,17 @@ package com.zabava.firebase
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.database.*
 import com.zabava.firebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+    private val myReference: DatabaseReference = database.reference.child("MyUsers")
+
+    val userList = ArrayList<Users>()
+    lateinit var usersAdapter: UsersAdapter
 
     private lateinit var mainBinding: ActivityMainBinding
 
@@ -21,5 +29,43 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        retrieveDataFromDatabase()
+
+    }
+
+    private fun retrieveDataFromDatabase() {
+
+        myReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                userList.clear()
+
+                for (eachUser in snapshot.children) {
+
+                    val user = eachUser.getValue(Users::class.java)
+
+                    if (user != null){
+
+                        println("userId: ${user.userId}")
+                        println("userName: ${user.userName}")
+                        println("userAge: ${user.userAge}")
+                        println("userEmail: ${user.userEmail}")
+                        println("****************************")
+
+                        userList.add(user)
+                    }
+
+                    usersAdapter = UsersAdapter(this@MainActivity, userList)
+
+                    mainBinding.rvMain.layoutManager = LinearLayoutManager(this@MainActivity)
+
+                    mainBinding.rvMain.adapter = usersAdapter
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 }
---
+++ app/src/main/java/com/zabava/firebase/UsersAdapter.kt
@@ -0,0 +1,37 @@
+package com.zabava.firebase
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.zabava.firebase.databinding.UsersItemBinding
+
+class UsersAdapter(
+    var context: Context,
+    var userList: ArrayList<Users>
+    ): RecyclerView.Adapter<UsersAdapter.UsersViewHolder>() {
+
+    inner class UsersViewHolder(val adapterBinding: UsersItemBinding)
+        : RecyclerView.ViewHolder(adapterBinding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UsersViewHolder {
+
+        val binding = UsersItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+
+        return UsersViewHolder(binding)
+
+    }
+
+    override fun onBindViewHolder(holder: UsersViewHolder, position: Int) {
+
+        holder.adapterBinding.tvName.text = userList[holder.adapterPosition].userName
+        holder.adapterBinding.tvAge.text = userList[holder.adapterPosition].userAge.toString()
+        holder.adapterBinding.tvEmail.text = userList[holder.adapterPosition].userEmail
+
+    }
+
+    override fun getItemCount(): Int {
+
+        return  userList.size
+    }
+}
