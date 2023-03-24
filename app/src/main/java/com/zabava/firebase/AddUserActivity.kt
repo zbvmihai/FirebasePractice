5cb22da58fe0861c8251f4a002d54fe63bdafbb0
Zabi <zbvmihai@gmail.com>
2023-03-24 22:42:36 +0000

    Added functionality to ask for media permission on API 33 devices

--- app/src/main/java/com/zabava/firebase/AddUserActivity.kt
+++ app/src/main/java/com/zabava/firebase/AddUserActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -56,23 +57,42 @@ class AddUserActivity : AppCompatActivity() {
 
     private fun chooseImage() {
 
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU){
 
-            ActivityCompat.requestPermissions(
-                this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1
-            )
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
 
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1
+                )
+
+            } else {
+
+                val intent = Intent()
+                intent.type = "image/*"
+                intent.action = Intent.ACTION_GET_CONTENT
+                activityResultLauncher.launch(intent)
+
+            }
         } else {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_MEDIA_IMAGES
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
 
-            val intent = Intent()
-            intent.type = "image/*"
-            intent.action = Intent.ACTION_GET_CONTENT
-            activityResultLauncher.launch(intent)
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(Manifest.permission.READ_MEDIA_IMAGES), 2
+                )
 
+            } else {
+
+                val intent = Intent()
+                intent.type = "image/*"
+                intent.action = Intent.ACTION_GET_CONTENT
+                activityResultLauncher.launch(intent)
+            }
         }
     }
 
@@ -81,20 +101,22 @@ class AddUserActivity : AppCompatActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if (requestCode == 1 && grantResults.isNotEmpty() && grantResults[0]
-            == PackageManager.PERMISSION_GRANTED) {
-
-            val intent = Intent()
-            intent.type = "image/*"
-            intent.action = Intent.ACTION_GET_CONTENT
-            activityResultLauncher.launch(intent)
-        } else {
-            Toast.makeText(
-                applicationContext,
-                "Permissions Denied. Go to application settings to enable it.",
-                Toast.LENGTH_SHORT
-            ).show()
+        when(requestCode) {
+            1,2 -> if (grantResults.isNotEmpty() && grantResults[0]
+                == PackageManager.PERMISSION_GRANTED) {
+                val intent = Intent()
+                intent.type = "image/*"
+                intent.action = Intent.ACTION_GET_CONTENT
+                activityResultLauncher.launch(intent)
+            }else {
+                Toast.makeText(
+                    applicationContext,
+                    "Permissions Denied. Go to application settings to enable it.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
+
     }
 
     private fun registerActivityForResult() {
--- app/src/main/java/com/zabava/firebase/MainActivity.kt
+++ app/src/main/java/com/zabava/firebase/MainActivity.kt
@@ -165,7 +165,7 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onCancelled(error: DatabaseError) {
-                    
+
                 }
 
             })
